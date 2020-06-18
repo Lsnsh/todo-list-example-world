d69ff3ac445c1e744c9b3705322f4a35466c9148
Lsnsh <zehexin6@163.com>
2020-06-18 16:09:49 +0800

    feat: (dart/flutter/todo_list_app) complete the todo page UI and part of the core logic

--- src/dart/flutter/todo_list_app/lib/src/pages/todo.dart
+++ src/dart/flutter/todo_list_app/lib/src/pages/todo.dart
@@ -1,6 +1,55 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class TodoPage extends StatelessWidget {
+class TodoPage extends StatefulWidget {
+  @override
+  TodoPageState createState() => TodoPageState();
+}
+
+class TodoPageState extends State<TodoPage> {
+  TextEditingController _nametextController;
+  TextEditingController _remarkextController;
+  var todoList = <TodoItem>[];
+
+  @override
+  void initState() {
+    super.initState();
+    _nametextController = TextEditingController(text: '');
+    _remarkextController = TextEditingController(text: '');
+    initTodoListState();
+  }
+
+  void initTodoListState() {
+    var initTodoList = """[
+      {
+        "title": "one",
+        "remark": "one' sub title."
+      },
+      {
+        "title": "two",
+        "remark": "two' sub title."
+      },
+      {
+        "title": "third",
+        "remark": "third' sub title."
+      },
+      {
+        "title": "four",
+        "remark": "four' sub title."
+      },
+      {
+        "title": "five",
+        "remark": "five' sub title."
+      }
+    ]""";
+
+    List<dynamic> list = jsonDecode(initTodoList);
+    list.forEach((item) {
+      todoList.add(TodoItem.fromJson(item));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +64,102 @@ class TodoPage extends StatelessWidget {
           )
         ],
       ),
-      body: Center(child: Text("this is todo page.")),
+      body: Wrap(
+        children: <Widget>[
+          _addTodoSection(),
+          Divider(),
+          _todoListSection(),
+        ],
+      ),
     );
   }
+
+  Widget _addTodoSection() {
+    return Column(
+      children: <Widget>[
+        TextField(
+            autofocus: true,
+            decoration: InputDecoration(
+                labelText: "Title",
+                hintText: "Please input title",
+                prefixIcon: Icon(Icons.title)),
+            controller: _nametextController),
+        TextField(
+            autofocus: true,
+            decoration: InputDecoration(
+                labelText: "Remark", prefixIcon: Icon(Icons.info_outline)),
+            controller: _remarkextController),
+        RaisedButton(
+            child: Text('Create'),
+            color: Colors.blue[500],
+            textColor: Colors.white,
+            onPressed: () {
+              print('current task title: ${_nametextController.text}');
+              print('current task remark: ${_remarkextController.text}');
+              setState(() {
+                todoList.add(TodoItem.fromJson({
+                  "title": _nametextController.text,
+                  "remark": _remarkextController.text
+                }));
+                _nametextController.clear();
+                _remarkextController.clear();
+              });
+            }),
+        RaisedButton(
+            child: Text('Print todo list JSON string'),
+            onPressed: () {
+              var jsonStr = jsonEncode(todoList);
+              print('current todo list json string:\n$jsonStr');
+            })
+      ],
+    );
+  }
+
+  Widget _todoListSection() {
+    return ListView.builder(
+        shrinkWrap: true,
+        itemCount: todoList.length,
+        itemBuilder: (context, i) {
+          var todoItem = todoList[i];
+          return ListTile(
+            title: Text(todoItem.title,
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 20,
+                )),
+            subtitle: Text(todoItem.remark),
+            leading: Icon(Icons.check_box_outline_blank),
+            onTap: () {
+              print('complete: ${todoItem.title}');
+              setState(() {
+                todoList.remove(todoItem);
+              });
+            },
+          );
+        });
+  }
+}
+
+class TodoItem {
+  String title;
+  String remark;
+
+  TodoItem(
+    this.title,
+    this.remark,
+  );
+
+  // TodoItem.fromJson(Map<String, dynamic> json)
+  //     : title = json['title'],
+  //       remark = json['remark'];
+  // The following code is the same as the above function
+  factory TodoItem.fromJson(Map<String, dynamic> json) {
+    return TodoItem(json['title'], json['remark']);
+  }
+
+  // jsonEncode method fallback
+  Map<String, dynamic> toJson() => {
+        'title': title,
+        'remark': remark,
+      };
 }
